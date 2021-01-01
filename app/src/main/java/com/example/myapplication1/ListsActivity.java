c3e7651dc68e725259dc9334cf6782571c7afc2b
kannan.m.hassouna@gmail.com <6*mAtBIEC6vhJzNi7Mr9UvjQaO7Czc4jr#mT7G@iNAPBnu2!9@uW$as3%^4>
2021-01-01 04:02:48 +0200

    store tasks list into firebase database

--- app/src/main/java/com/example/myapplication1/ListsActivity.java
+++ app/src/main/java/com/example/myapplication1/ListsActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -15,9 +16,15 @@ import android.widget.Toast;
 import com.example.myapplication1.models.Task;
 import com.example.myapplication1.models.TasksList;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class ListsActivity extends AppCompatActivity {
 
@@ -26,30 +33,44 @@ public class ListsActivity extends AppCompatActivity {
     private java.util.List todoLists = new ArrayList<>();
     private java.util.List searchedToDoList = new ArrayList<>();
     private FirebaseAuth mAuth;
+    private FirebaseUser user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lists);
         mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
         findViewById(R.id.back).setOnClickListener(V -> onBackPressed());
         findViewById(R.id.create_list).setOnClickListener(V -> showNewListDialog());
         findViewById(R.id.logout).setVisibility(View.VISIBLE);
         findViewById(R.id.logout).setOnClickListener(V -> logout());
         listRecyclerView = findViewById(R.id.lists_recycler_view);
 
-        TasksList todoTasksList = new TasksList();
-        todoTasksList.setTitle("ahmed");
-        Task todo = new Task();
-        todo.setTitle("Ahmed");
-        HashMap<String, Task> td_list = new HashMap<>();
-        td_list.put("1", todo);
-
-        todoTasksList.setTasks(td_list);
-        todoLists.add(todoTasksList);
         listsAdapter = new ListsAdapter(this, todoLists);
         listRecyclerView.setAdapter(listsAdapter);
         listRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //      Firebase  Snapshot
+
+        if(user != null)
+        FirebaseDatabase.getInstance().getReference("Users").child(user.getUid()).child("lists").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                todoLists.clear();
+                for(DataSnapshot snapShot: dataSnapshot.getChildren()) {
+                    todoLists.add(snapShot.getValue(TasksList.class));
+                }
+                listsAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
+
     private void showNewListDialog() {
         final EditText listEditText = new EditText(this);
         listEditText.setHint("List name");
@@ -61,15 +82,23 @@ public class ListsActivity extends AppCompatActivity {
                 .create();
         dialog.show();
         dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(v -> {
-           if (!listEditText.getText().toString().trim().equals("")) {
+            if (!listEditText.getText().toString().trim().equals("")) {
                 dialog.dismiss();
+                String uid = mAuth.getCurrentUser().getUid();
+                HashMap<String, Object> data = new HashMap<>();
+                HashMap<String, Task> list = new HashMap<String, Task>();
+                data.put("tasks", list);
+                data.put("title", listEditText.getText().toString());
+                FirebaseDatabase.getInstance().getReference("Users").child(uid).child("lists").push().setValue(data);
+                Toast.makeText(this, "Added Successfully", Toast.LENGTH_LONG).show();
             }
 
         });
     }
 
-    private void logout(){
+    private void logout() {
         mAuth.getInstance().signOut();
+        Toast.makeText(this, "Signed out", Toast.LENGTH_LONG);
         startActivity(new Intent(this, login.class));
     }
 }
--- app/src/main/java/com/example/myapplication1/models/Task.java
+++ app/src/main/java/com/example/myapplication1/models/Task.java
@@ -6,11 +6,6 @@ public class Task {
     private String title;
     private boolean isChecked;
     private String description;
-    private String type = "Personal";
-    private long time;
-
-    public Task() {
-    }
 
     public String getId() {
         return id;
@@ -42,21 +37,5 @@ public class Task {
 
     public void setDescription(String description) {
         this.description = description;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public long getTime() {
-        return time;
-    }
-
-    public void setTime(long time) {
-        this.time = time;
     }
 }
